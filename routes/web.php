f406fca209703ad23f96a2ea1d59225eb5d098fb
202545ploy <ploychatput@gmail.com>
2023-05-31 16:08:36 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+   
+
+    Route::get('/', function () {
+    return view('welcome');
+});
+    Route::get('/about', function () {
+   // return ('about page');
+     return view('about');
+});
+
+
+Route::get('/post', function () {
+   // return 'Post Page';
+    return view ('post');
+});
+
+Route::get('/profile', function () {
+   // return "Profile Page";
+   return view ('profile');
+});
+
+Route::get('/contact', function () {
+   return view ('contact');
+});
+
+Route::view('link','/link');
+
+
+    
